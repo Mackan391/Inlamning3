88ac83c1cdcc5a6f11ec92cf6230bb315ca58a44
Marcus Eriksson <erikssonmarcus48@gmail.com>
2025-11-03 10:27:32 +0100

    Initial commit

---
+++ src/FemtonSpel.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FemtonSpel extends JFrame implements ActionListener {
+
+    public FemtonSpel() {
+        super("Välkommen till Femtonspelet!");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 400);
+        setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    void main() {
+
+    }
+}
